03ce976ad1456b7dc466736c67ae5eb02a4adfe3
tmpUsername <admin@fddf-PC>
2016-10-04 16:20:43 +0200

    PersonneDAO.java: ajout du dao pour la classe Personne(+Test)

---
+++ StreamingMavenSpringBoot/src/main/java/streaming/dao/PersonneDAO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import streaming.entity.Personne;
+
+/**
+ *
+ * @author admin
+ */
+@Repository
+public class PersonneDAO {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    public List<Personne> lister(){
+        return em.createQuery("SELECT p FROM Personne p").getResultList();
+    }
+    
+    @Transactional
+    public void ajouter(Personne p){
+        em.persist(p);
+    }
+}
